fe47e772ee2e0a2282bacfd5edb1f625e18ad396
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-11-14 18:41:32 +0200

    Edit helloWorld.spec.js and helloWorld.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function (array, deleteElement) {
-
+const removeFromArray = function (array, ...deleteElement) {
+    for (let i = 0; i < deleteElement.length; i++) {
+        if (array.includes(deleteElement[i])) {
+            array.splice(array.indexOf(deleteElement[i]), 1);
+        }
+    }
+    return array;
 };
+
+const randomArray = [1, 4];
+console.log(removeFromArray(randomArray, 2, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let sum = 0
+    if (num1 >= 0 && num2 >= 0 && typeof num1 === "number" && typeof num2 === "number") {
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        }
 
+        else if (num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    else {
+        return "ERROR"
+    }
 };
+
+console.log(sumAll(4, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
