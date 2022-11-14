fbc07a6dbd8a3103064e651957cd0ded1d070252
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-11-14 21:45:25 +0200

    Edit tempConversion.js and tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,25 @@
-const ftoc = function() {
-
+const ftoc = function (fehr) {
+  let cil = (fehr - 32) * (5 / 9);
+  if (cil - Math.floor(cil) !== 0) {
+    return +Number(cil).toFixed(1);
+  }
+  else {
+    return cil;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function (cil) {
+  let fehr = (9 / 5 * cil) + 32
+  if (fehr - Math.floor(fehr) !== 0) {
+    return +Number(fehr).toFixed(1);
+  }
+  else {
+    return fehr;
+  }
 };
+
+console.log(ftoc(-1));
+console.log(ctof());
 
 // Do not edit below this line
 module.exports = {
