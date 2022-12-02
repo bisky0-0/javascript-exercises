5f94b7501a5707659e97d41deae7f954d06f66bc
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-12-02 14:50:24 +0200

    Edit calculator.js and calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce(function (total, newNum) {
+    return total + newNum;
+  }, 0)
+}
+
+const multiply = function (array) {
+  return array.reduce(function (finalVal, num) {
+    return finalVal * num
+  }, 1)
 };
 
-const multiply = function() {
+const power = function (a, b) {
+  return a ** b;
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  for (let i = num - 1; i > 0; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
