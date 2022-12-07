d9cc43735e79ebe8ff917897d8c33a85d569846d
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-12-07 22:55:14 +0200

    Edit findTheOldest.spec.js and findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let age;
+    people.map(function (elem) {
+        if (elem.yearOfDeath) {
+            console.log(elem.yearOfDeath - elem.yearOfBirth)
+            return elem.age = elem.yearOfDeath - elem.yearOfBirth;
+        }
+        else {
+            console.log(2022 - elem.yearOfBirth)
+            return elem.age = 2022 - elem.yearOfBirth;
+        }
+    });
+    people.sort(function (a, b) {
+        if (a.age > b.age) {
+            return -1;
+        }
+        else if (a.age < b.age) {
+            return 1
+        }
+        else {
+            return 0;
+        }
+    });
 
+    return people[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
