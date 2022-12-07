dd49297ed6dbd56ca987b6e8c337cad7028658ac
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-12-07 20:00:00 +0200

    Edit getTheTitles.spec.js and getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (book) {
+    return book.map(function (elem) {
+        return elem.title;
+    })
 };
 
 // Do not edit below this line
