08aa17abc172ab9210f93a50fbcdb3835211a4f1
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-12-07 19:37:38 +0200

    Edit fibonacci.spec.js and fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    if (index >= 0) {
+        let num = 0;
+        let array = [0, 1];
+        for (let i = 2; i <= index; i++) {
+            num = array[i - 1] + array[i - 2];
+            array.push(num);
+        }
+        return array[index];
+    }
+    else {
+        return "OOPS"
+    }
 };
+console.log(fibonacci("5"))
 
 // Do not edit below this line
 module.exports = fibonacci;
