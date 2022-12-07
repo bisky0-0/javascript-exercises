9b4533e429233d4d57a22b03928479693fed9b7e
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-12-07 16:34:21 +0200

    Edit add palindromes.spec.js and palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    let arrStr = Array.from(str);
+    const specialChars = ` /.[]{};:'",!@#$%^&*()_+=-<>?`;
+    const specialCharsArray = specialChars.split('');
+    let specialCharsArrayLength = specialCharsArray.length;
+    for (let i = 0; i < specialCharsArrayLength; i++) {
+        if (arrStr.includes(specialCharsArray[i])) {
+            while (arrStr.indexOf(specialCharsArray[i]) != -1) {
+                arrStr.splice(arrStr.indexOf(specialCharsArray[i]), 1)
+            }
+        }
+    }
+    if (arrStr.join("").toLocaleLowerCase() === arrStr.reverse().join("").toLowerCase()) {
+        return true
+    }
+    return false;
+}
+console.log(palindromes("hello!.,<"))
 // Do not edit below this line
 module.exports = palindromes;
